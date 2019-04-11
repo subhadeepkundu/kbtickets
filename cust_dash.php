67c746907566ec6ba8469b1b31c49480bf3c9dee
subhadeepkundu <subhadeepkundu98@gmail.com>
2019-04-11 08:34:12 +0530

    successful payment added

--- cust_dash.php
+++ cust_dash.php
@@ -110,109 +110,6 @@ $email= $_SESSION["email"]; $results = mysqli_query($conn, "SELECT * FROM custom
 	<!-- Page top section end -->
 
 
-	<!-- Portfolio section start -->
-	<!-- <section class="portfolio-section">
-		<div class="sp-pad spad"> -->
-			<!-- portfolio filter menu -->
-			<!-- ul class="portfolio-filter controls">
-				<li class="control" data-filter="*">All</li>
-				<li class="control" data-filter=".photo">Photography</li>
-				<li class="control" data-filter=".wedding">Weddings</li>
-				<li class="control" data-filter=".land">Landscapes</li>
-				<li class="control" data-filter=".port">Portraits</li>
-			</ul>
-		</div> -->
-		<!-- <div class="portfolio-warp"> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio set-bg photo" data-setbg="img/portfolio/1.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/1.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio set-bg wedding" data-setbg="img/portfolio/2.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/2.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio set-bg land" data-setbg="img/portfolio/3.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/3.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio sm-wide set-bg port" data-setbg="img/portfolio/4.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/4.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio sm-wide set-bg photo" data-setbg="img/portfolio/5.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/5.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio set-bg wedding" data-setbg="img/portfolio/6.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/6.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio sm-wide set-bg land" data-setbg="img/portfolio/7.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/7.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio set-bg port" data-setbg="img/portfolio/8.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/8.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio set-bg photo" data-setbg="img/portfolio/9.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/9.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div> -->
-			<!-- single item -->
-			<!-- <div class="mix single-portfolio set-bg wedding" data-setbg="img/portfolio/10.jpg">
-				<a href="#" class="portfolio-info">
-					<div class="pfbg set-bg" data-setbg="img/portfolio/10.jpg"></div>
-					<h5>Summer in the desert</h5>
-					<p>Landscape Photography</p>
-				</a>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-		<div class="sp-pad next-portfolio-link">
-			<a href="#" class="arrow-btn">
-				<i class="fa fa-angle-right"></i>
-				<i class="fa fa-angle-right"></i>
-				<i class="fa fa-angle-right"></i>
-			</a>
-		</div>
-	</section> -->
 <?php
 $AC1=array("Jadavpur","Jadavpur P.S.","South City/Lake Gardens","Prince Anwar Shah Road","Rashbehari Avenue","Hazra Park","Elgin Road","Rabindra Sadan/Exide","Park Street","Esplanade","B.B.D. Bag","Howrah Bridge East","Howrah Station");
 $AC9B=array("Jadavpur","Sukanta Setu","Santoshpur","Survey Park/Kalikapur","Kasba Depot/Ruby Hospital","Panchanna Gram/VIP Nagar Bazar","Science City/Park Circus Connector","Metropolitan Co-Op Housing Estates","Chingrihata","Sukanta Nagar","Nicco Park","Swastha Bhavan","College Mor","Technopolis","New Town Bus Stand","HIDCO Bhavan/Narkel Bagan","Tata Medical Centre","Ecospace");
@@ -246,16 +143,7 @@ $all=array("Jadavpur","Jadavpur P.S.","South City/Lake Gardens","Prince Anwar Sh
 	        }
 	        ?>
 		</select><br><br>
-		<!-- <select class="mdb-select md-form" searchable="Search here..">
-		  	<option value="" disabled selected>Enter Fare per ticket</option>
-		  	<?php 
-			for($i=1; $i<=50; $i++)
-			{
-			    echo "<option value=".$i.">".$i."</option>";
-			}
-			?> 
-			<option name="fare"> </option>
-		</select>-->
+		
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<select class="mdb-select md-form" searchable="Search here.." name="qty">
 		  	<option value="" disabled selected>Enter Quantity</option>
 		  	<?php 
@@ -363,6 +251,8 @@ $delimiter=',';
 
 <?php
  if(isset($_POST['submit1'])){
+ 	$message = "Your Payement Was Successful!!!";
+	echo "<script type='text/javascript'>alert('$message');</script>";
  	$start= $_POST['start'];
  	$stop= $_POST['stop'];
  	$routeSelected= $_POST['routeSelected'];
